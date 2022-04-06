cce9fb29ab03371d007e1c012b45a50cd6de3b93
Aashish Sharma <ashishchapain86@gmail.com>
2022-04-06 12:19:46 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### Socket Programming with Python
---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.1.26"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello World!")
+input()
+send("Hello Everyone!")
+input()
+send("Hello Aashish!")
+
+send(DISCONNECT_MESSAGE)
